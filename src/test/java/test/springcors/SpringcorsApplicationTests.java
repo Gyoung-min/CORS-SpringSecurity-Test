034133832e8e89ce34c37f81e82d47b0f4fc4f5d
Min <rudals0326@gmail.com>
2022-08-25 04:56:39 +0900

    initialize project

---
+++ src/test/java/test/springcors/SpringcorsApplicationTests.java
@@ -0,0 +1,13 @@
+package test.springcors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
